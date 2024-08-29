05a0e3152a2c07f025ff6d141aa08c01e705c0e3
Reesejr014 <reesejr014@gmail.com>
2024-08-29 11:44:01 -0400

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,20 @@
+name = "Georgia"
+age = "3.5"
+hair_color = "brown" #use quotations on a string of text
+
+print(name)
+
+def hair_color_function():
+  if hair_color == "yellow":
+    print("Youve got yellow")
+  else:
+    print("you dont have yellow hair")
+    
+  
+  
+hair_color_function()
+  
+sentencemerge = name + " " +"is" + " " + age
+print(sentencemerge)
+  
+  
